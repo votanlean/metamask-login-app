acc365a1eefc3bc831c88d96d306a7e3e473d3c3
Alex Vo <votanlean@gmail.com>
2023-01-22 23:53:05 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
 function App() {
+  const signMessage = async () => {
+    if (!window.ethereum)
+      throw new Error("No crypto wallet found. Please install it.");
+    await window.ethereum.send("eth_requestAccounts");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    const address = await signer.getAddress();
+
+    fetch(`http://localhost:8000/get-nonce?address=${address}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const nonce = data.nonce;
+        const message = `Welcome! Click to sign in
+
+Nonce: ${nonce}`;
+        signer.signMessage(message).then((signature) => {
+          fetch("http://localhost:8000/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              signature: signature,
+              address: address,
+            }),
+          })
+            .then((response) => response.json())
+            .then((data) => console.log("data", data));
+        });
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={signMessage}>Sign Message</button>
     </div>
   );
 }
